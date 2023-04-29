6c6e22bfe439feeb2ab2a4d7a30ff4b70ea7191a
Akbar Ahmadjonov <akbarahmadjonovv@mail.ru>
2023-04-29 12:21:41 +0500

    Add fonts directly

--- src/widgets/header/Header.jsx
+++ src/widgets/header/Header.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import header from "./header.scss";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return <div>Header</div>;
+};
 
-export default Header
+export default Header;
