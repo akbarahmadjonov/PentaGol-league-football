341e2524767a55dc96a8fb6e984e2feb7f13bd45
Akbar Ahmadjonov <akbarahmadjonovv@mail.ru>
2023-04-29 16:55:24 +0500

    commited all changes

--- src/widgets/header/Header.jsx
+++ src/widgets/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import style from "./style.scss";
+import Logo from "../../shared/assets/images/logo.svg";
+import DarkMode from "../../shared/assets/images/dark-mode.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="header">
+        <div className="container">
+          <div className="imageWrapper">
+            <div>
+              <img src={Logo} alt="Logo" />
+            </div>
+            <div>
+              <button>
+                <img src={DarkMode} alt="Dark mode" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
